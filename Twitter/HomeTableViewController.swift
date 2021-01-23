ef96b74a89cfbd15fb39d482dc18d3f3e2ffd2b5
Saul Fernandez <fsaul16@yahoo.com>
2021-01-22 17:48:15 -0800

    Login fixes

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -73,7 +73,7 @@ class HomeTableViewController: UITableViewController {
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
-        UserDefaults.standard.set(false, forKey: "userLoggedin")
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
